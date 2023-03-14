48c602346bf7090da9442e9d366b8b3260c11871
Divy0409 <divypatel125710@gmail.com>
2023-03-14 15:12:16 +0530

    Create using.py

---
+++ Python/Labwork/using.py
@@ -0,0 +1,11 @@
+import random
+
+
+random_number = random.randint(0,5)
+# print(random_number)
+random_float = random.random() #*100
+# print(random_float)
+
+lst = ["Python","Java","Html","Javascript"]
+choice = random.choice(lst)
+print(choice)
